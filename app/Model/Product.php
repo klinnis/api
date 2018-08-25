009de70f921efb41fa536ddd940f4863fd2039fb
klinnis <markos.klinakis@gmail.com>
2018-08-25 12:12:02 +0300

    Relationship ready

--- app/Model/Product.php
+++ app/Model/Product.php
@@ -6,5 +6,20 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable=[
+
+        'name',
+        'detail',
+        'price',
+        'stock',
+        'discount'
+
+    ];
+
+    public function review(){
+
+        return $this->hasMany(Review::class);
+
+    }
+
 }
--- app/Model/Review.php
+++ app/Model/Review.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    protected $fillable=[
+
+        'product_id',
+        'customer',
+        'review',
+        'star'
+    ];
+
+    public function product(){
+
+        return $this->belongsTo(Product::class);
+
+    }
 }
